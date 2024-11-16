f9a32371a73bbfefb3de7d3f296e67fd63f25b15
xezernn <xezernovruz79@gmail.com>
2024-11-17 01:45:14 +0400

    edit whit img end points

--- server.js
+++ server.js
@@ -1,21 +1,16 @@
-// JSON Server module
+const path = require('path');
 const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db/db.json");
 const middlewares = jsonServer.defaults();
 
+// Şəkil qovluğunun yerini göstərin
+server.use('/img', jsonServer.defaults({ static: path.join(__dirname, 'db/img') }));
+
 server.use(middlewares);
-// Add this before server.use(router)
-server.use(
-	// Add custom route here if needed
-	jsonServer.rewriter({
-		"./*": "/$1",
-	})
-);
 server.use(router);
 server.listen(3000, () => {
-	console.log("JSON Server is running");
+    console.log("JSON Server is running on http://localhost:3000");
 });
 
-// Export the Server API
 module.exports = server;
